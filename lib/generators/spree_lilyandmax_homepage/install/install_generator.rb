45d903f5fec0b793344af5f1f72e12add440a91e
Sam <samtreweek@gmail.com>
2012-04-27 12:32:39 +0100

    updated some references to paths

--- lib/generators/spree_lilyandmax_homepage/install/install_generator.rb
+++ lib/generators/spree_lilyandmax_homepage/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeLilyandmaxHomepage
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require spree/spree_lilyandmax_homepage\n"
+        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_lilyandmax_homepage\n"
         append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_lilyandmax_homepage\n"
         append_file "app/assets/javascripts/store/all.js", "//= require store/jquery.cycle.all\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require spree/spree_lilyandmax_homepage\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_lilyandmax_homepage\n", :before => /\*\//, :verbose => true
         inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_lilyandmax_homepage\n", :before => /\*\//, :verbose => true
       end
 
