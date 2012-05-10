b2a69c027db3a312107779a5bf0f2054bfe4009d
Sam <samtreweek@gmail.com>
2012-05-10 14:57:48 +0100

    changed food ideas to cupcake decorations, checkbox on products will now display on cupcake decorations page

--- app/overrides/add_cupcake_decorations_to_homepage.rb
+++ app/overrides/add_cupcake_decorations_to_homepage.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/home/index",
-                    :name => "add_food_ideas_to_homepage",
+                    :name => "add_cupcake_decorations_to_homepage",
                     :insert_top => "[data-hook='homepage_products']",
-                    :partial => 'spree/shared/food_ideas',
+                    :partial => 'spree/shared/cupcake_decorations',
                     :disabled => false)
