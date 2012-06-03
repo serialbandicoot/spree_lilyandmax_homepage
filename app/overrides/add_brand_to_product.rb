24903a356ba6938c7cfae92bafb3eeec7a6cb52b
Sam <samtreweek@gmail.com>
2012-06-03 08:13:30 +0100

    small refactor on naming and correct partial path

--- app/overrides/add_brand_to_product.rb
+++ app/overrides/add_brand_to_product.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
-                    :name => "add_brand_to_product",
+                    :name => "add_brand_to_product_admin",
                     :insert_after => "[data-hook='admin_product_form_right']",
-                    :partial => "spree/shared/_add_brand_to_product",
+                    :partial => "spree/shared/add_brand_to_product_admin",
                     :disabled => false)
