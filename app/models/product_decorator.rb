f8a70e16683b4d2cff08cc0734bce94121e736dc
Sam <samtreweek@gmail.com>
2013-01-09 19:19:35 +0000

    put a check in for deleted products, featured_products, cupcake_decoration and product_on_homepage boolean should really be removed

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Product.class_eval do
   scope :on_homepage
-  attr_accessible :brand, :show_on_homepage, :feature_product, :spree_additional_info
+  attr_accessible :brand, :show_on_homepage, :feature_product, :spree_additional_info, :cupcake_decoration
 end
