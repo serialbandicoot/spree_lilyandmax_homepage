40228a898698d8a7aef4804f50c3a86a4fc9a9b9
Sam <samtreweek@gmail.com>
2013-04-26 19:45:33 +0100

    added product property assignment

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Product.class_eval do
   scope :on_homepage
-  attr_accessible :brand, :show_on_homepage, :feature_product, :spree_additional_info, :cupcake_decoration, :name, :permalink, :description, :price, :cost_price, :available_on, :sku, :on_hand, :weight, :height, :width, :depth, :shipping_category_id, :tax_category_id, :meta_keywords, :meta_description
+  attr_accessible :brand, :show_on_homepage, :feature_product, :spree_additional_info, :cupcake_decoration, :name, :permalink, :description, :price, :cost_price, :available_on, :sku, :on_hand, :weight, :height, :width, :depth, :shipping_category_id, :tax_category_id, :meta_keywords, :meta_description, :product_properties_attributes
 end
